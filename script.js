f14ebf3dec24aacfbff695c262d7e2db0c0d028a
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-04-30 09:41:18 -0400

    Add getComputerChoice()

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ function getComputerChoice() {
     if (n <= 1/3) {
         rlst = "rock";
     }
-    else if (n>=2/3) {
+    else if (n>2/3) {
         rlst = "scissors";
     }
     else { rlst = "paper"}
     return rlst;
+}
+
+function getHumanChoice() {
+    let input = prompt('Pick Rock Paper or Scissors');
+    let a = (input.trim()).toLowerCase();
+    let b ='invalid input';
+    if (a == 'rock') {b ='rock'}
+    else if (a == 'paper') {b ='paper'}
+    else if (a == 'scissors'){b ='scissors'}
+    return b;
 }
